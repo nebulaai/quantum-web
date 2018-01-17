4f1ab6543a2d24afbb2b1a2f4d44f337976c6623
tong.pang@mail.mcgill.ca <abcd1234>
2018-01-17 15:30:25 -0500

    ABI update for nebula ai contract v 0.2

--- web/assets/js/contract.js
+++ web/assets/js/contract.js
@@ -1,23 +1,17 @@
 window.onload = function () {
-    let isChrome = !!window.chrome && !!window.chrome.webstore;
-    let isSafari = /constructor/i.test(window.HTMLElement)
-        || (
-            function (p) {
-                return p.toString() === "[object SafariRemoteNotification]";
-            })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+    // let isChrome = !!window.chrome && !!window.chrome.webstore;
 
-    if(!isChrome&&!isSafari){
-        window.open("../template/InvalidBrowser.html", "_self");
-    }
+    // if(!isChrome){
+    //     window.open("../template/InvalidBrowser.html", "_self");
+    // }
 };
 
-// const contractAddress = "0xa38effc32e43abe7c5c320899f084338194e4c8b"; //Helix v1.0
-const contractAddress = "0xd6e754b380863c725e7b99cf1b6a0d4d3e0e6619"; //Helix v1.1
+const contractAddress = "0xe4c0e78969f19b9629662bcd8cf8e1fb1b73d749"; //Helix v0.2
 
 
-const loadContract = function() {
+const loadContract = function(callback) {
     $.ajax({
-        url: "static/ABI/NebulaAi1.1.json",
+        url: "../assets/ABI/NebulaAi1.json",
         dataType: "json",
         error: function (e) {
             console.log(e);
@@ -25,27 +19,68 @@ const loadContract = function() {
         success: function (data) {
             let NebulaAi = web3.eth.contract(data);
             window.nebulaAi = NebulaAi.at(contractAddress);
-            $("#contractReady").show();
+            console.log("current nebula base contract @ ", contractAddress);
 
+            $("#contractReady").show();
+            callback();
         }
     })
 };
-
-const initiateContract = function () {
-    if (typeof web3 !== 'undefined') {
-        web3 = new Web3(web3.currentProvider);
-        //Currently only support Chrome with MetaMask plugin installed
-        //loginWallet using private key or JSON format
-
-        if (web3.eth.defaultAccount === undefined) {
-            alert("Please log into your MetaMask account using Chrome MetaMask Plugin");
+const prepareTaskContract = function(){
+    $.ajax({
+        url: "../assets/ABI/Task_1.json",
+        dataType: "json",
+        error: function (e) {
+            console.log(e);
+        },
+        success: function (data) {
+            window.taskContract = web3.eth.contract(data);
         }
-
-        loadContract();
-    } else {
-        alert('No web3? You should consider trying MetaMask! Redirecting to loginWallet page...');
-        window.open('need_MetaMask.html', '_self');
-    }
+    });
+};
+const loadTaskContract = function(taskAddress){
+    window.taskContractInstance = window.taskContract.at(taskAddress);
 };
 
-initiateContract();
+const initiateContract = function (callback) {
+
+    window.addEventListener('load', function() {
+
+        // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+        if (typeof web3 !== 'undefined') {
+            //Currently only support Chrome with MetaMask plugin installed
+            //loginWallet using private key or JSON format
+            web3 = new Web3(web3.currentProvider);
+
+            if (web3.eth.defaultAccount === undefined) {
+                alert("Please log into your MetaMask account using MetaMask Plugin");
+            }
+            callback();
+
+        } else {
+            console.log('No web3? You should consider trying MetaMask!')
+            // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+            web3 = new Web3(new Web3.providers.HttpProvider("http://ec2-18-221-29-136.us-east-2.compute.amazonaws.com:8545"));
+        }
+
+        // // Now you can start your app & access web3 freely:
+        // startApp()
+
+    })
+
+    // if (typeof web3 !== 'undefined') {
+    //     web3 = new Web3(web3.currentProvider);
+    //     //Currently only support Chrome with MetaMask plugin installed
+    //     //loginWallet using private key or JSON format
+    //
+    //     if (web3.eth.defaultAccount === undefined) {
+    //         alert("Please log into your MetaMask account using MetaMask Plugin");
+    //     }
+    //     callback();
+    //
+    //
+    // } else {
+    //     alert('No web3? You should consider trying MetaMask! Redirecting to loginWallet page...');
+    //     // window.open('need_MetaMask.html', '_self');
+    // }
+};
--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -1,26 +1,12 @@
-const taskServerUrl = "http://192.168.88.187:8080/nebula-task/task/";
+initiateContract(function () {
+    loadContract(function () {});
+    prepareTaskContract();
+});
 
-const minimalFee = 2.06;
+const scriptAddressDefault = "http://ec2-18-218-114-50.us-east-2.compute.amazonaws.com:8080/script/Nebula_LSTM.py";
+const outputAddress = "http://ec2-18-218-114-50.us-east-2.compute.amazonaws.com:8080/nebula-quantum/create/";
 
-
-//
-// /**
-//  * Event log class, for all events listening
-//  * @param result
-//  * @constructor
-//  */
-// function EventLog(result) {
-//     this.contractAddress = result.address;
-//     this.blockHash = result.blockHash;
-//     this.blockNumber = result.blockNumber;
-//     this.event = result.event;
-//     this.logIndex = result.logIndex;
-//     this.transactionHash = result.transactionHash;
-//     this.transactionIndex = result.transactionIndex;
-//     this.args = result.args;
-//     this.removed = result.removed;
-// }
-
+const minimalFee = 5;
 
 /**
  * Temporary solution, this must move to backend
@@ -33,149 +19,337 @@ const uuidv4 = function() {
 };
 
 /**
- * TODO 1. the gas here should be a constant value, since it is the same process every time
- * TODO 2. Some of the fee calculation can be done in frontend to save gas fee (safety???)
- * TODO 3. Base fee calculator, should be done somewhere other than in contract
- *
- * This is scratch version of the submit function
+ * Order detail parameter
+ * @type {Order}
  */
-let currentOrder = {};
-
-const loadPreTrainedPage = function () {
-
+let currentOrder;
+const blocks = {
+    submit : {},
+    task : {
+        task_id:0,
+        uuid:"",
+        worker:"",
+        has_issue:false,
+        started:false,
+        completed:false
+    },
+    dispatch : {},
+    start : {},
+    complete : {}
 };
 
-const uploadData = function (callback) {
-    //choose file
-    //upload
-    //show progress
-    //display the uploaded uri in data field
-
+const useDefault = function () {
+    $("#dataUri").val(scriptAddressDefault);
 };
-function Order(uuid, datasetUri, name, script, output, data) {
+
+const toggleUploadPanel = function () {
+    $("#btn-group-1").slideToggle();
+    $("#uploadPanel").slideToggle();
+};
+
+
+function Order(uuid, name, datasetUri, script, output, param) {
     this.uuid = uuid;
-    this.datasetURI = datasetUri;
     this.name = name;
-    this.script = script;
+    this.datasetURI = datasetUri;
+    this.scriptURI = script;
     this.outputURI = output;
-    this.data = data;
-    this.fee = 2.06;
+    this.parameters = param;
+    this.fee = 10 * 10^18;
     this.progress = 0;
     this.status = 0;
     this.transactionHash = "";
+    this.taskContractAddress = "";
 }
 Order.prototype.setTransactionHash = function (hash) {
     this.transactionHash = hash;
 };
+Order.prototype.setTaskContractAddress = function (address) {
+    this.taskContractAddress = address;
+};
+
 
 const submitOrder = function (callback) {
     let uuid = uuidv4();
-    let dataUri = $("#dataUri").val();
-    let scriptAddress = $("#script").val();
-    let outputAddress = $("#output").val();
+    // let dataUri = $("#dataUri").val();
     let taskName = $("#taskName").val();
-    let data = { "epoch" : $("#epoch").val() };
+    let params = { "epoch" : $("#epoch").val() };
+    let scriptAddress = $("#dataUri").val();
 
     currentOrder = new Order(
         uuid,
-        dataUri,
         taskName,
+        "",
         scriptAddress,
         outputAddress,
-        data
+        params
     );
 
-    $.ajax({
-        type: 'POST',
-        url: taskServerUrl,
-        data: JSON.stringify({
-            "uuid": uuid,
-            "datasetURI": dataUri,
-            "name": taskName,
-            "script": scriptAddress,
-            "outputURI": outputAddress,
-            "data": JSON.stringify(data)
-        }),
-        dataType: "json",
-        contentType: "application/json",
-        error: function (e) {
-            console.log(e);
-        },
-        success: function(){
-            callback();
-        }
-    })
+    callback();
 };
-const attachLink = function (type, hash) {
-    $("#payment")
-        .append("<div>Task ( " + currentOrder.name + " ) " + type + " ... <div>")
-        .append("<div>Transaction Hash: <a href='http://192.168.88.192:8000/#/tx/" + hash +"' target='_blank'>"+hash+"</a></div>");
 
+$(document).ready(function () {
+    $('.data-form').on('submit', function (e) {
+        e.preventDefault();
+        submitOrder(function () {
+            $("#payment").show();
+            $("#create_task_btn").hide();
+        });
+    });
+});
+
+const waitingForSubmitConfirmation = function (result) {
+
+    currentOrder.setTransactionHash(result);
+
+    let txHash = result;
+    getTransaction("#transactionHash", blocks.submit, txHash);
+
+    console.log("Transaction Hash: ", result);
+    console.log("waiting for submit confirmation");
+
+    let submitEvent = nebulaAi.TaskSubmitted();
+
+
+    submitEvent.watch(function (error, result) {
+        if(result.args._sender_address.toLowerCase() === web3.eth.defaultAccount.toLowerCase()) {
+            submitEvent.stopWatching();
+            if (error) {
+                console.log("error: ", error);
+            } else {
+                getTransaction("#transactionHash", blocks.submit, txHash);
+                currentOrder.setTaskContractAddress(result.args._task_address);
+                loadTaskContract(result.args._task_address);
+
+                $("#taskReceived").show();
+                $("#task_add_cell").empty().html(createLinkToExplorer(result.args._task_address, "address"));
+
+                getTaskID();
+                getUuid();
+                myTaskWorker();
+                taskContractInstance.task_issue(function (error, result) {
+                    if (error) {
+                        console.log(error);
+                        $('#task_ok_cell').empty().html('error');
+                        $('#task_issue_txhash').empty().html(error ? "ERROR_HASH" : "N/A");
+                    } else {
+                        $('#task_ok_cell').empty().html('ok');
+                        blocks.task.has_issue = result;
+                    }
+                });
+
+                console.log("Task submitted @ address : ", currentOrder.taskContractAddress);
+
+                // waitingForTaskDispatch();
+            }
+        }
+    });
+};
+const waitingForTaskDispatch = function () {
+
+    console.log("waiting for task @ ", currentOrder.taskContractAddress, " to be dispatched");
+
+    let dispatchEvent = nebulaAi.TaskDispatched();
+
+    dispatchEvent.watch(function (error, result) {
+        if (error) {
+            console.log(error);
+        } else {
+            if (result.args._sender_address.toLowerCase() === web3.eth.defaultAccount.toLowerCase()) {
+
+                dispatchEvent.stopWatching();
+                $("#taskDispatched").show();
+
+
+                $("#dispatch_block").html(createLinkToExplorer(result.blockNumber,"block"));
+                $("#dispatch_block_hash").html(createLinkToExplorer(result.blockHash,"block"));
+                $("#task_disp_txhash").html(createLinkToExplorer(result.transactionHash,"tx"));
+                myTaskWorker();
+            }
+        }
+    });
+};
+const waitingForTaskStart = function(){
+    //wait for start
+    console.log("Waiting for task @ ", currentOrder.taskContractAddress, " to start");
+    let startEvent = nebulaAi.TaskConfirmed();
+    startEvent.watch(
+        function (error, result) {
+            if (error) {
+                console.log(error);
+            } else {
+                if(result.args._task_owner.toLowerCase() === web3.eth.defaultAccount.toLowerCase()){
+                    startEvent.stopWatching();
+                    $('#taskStarted').show();
+                    console.log('Task '+currentOrder.taskContractAddress+' started');
+                    $('#start_block').html(createLinkToExplorer(result.blockNumber,"block"));
+                    $('#start_block_hash').html(createLinkToExplorer(result.blockHash,"tx"));
+                    $('#task_start_txhash').html(createLinkToExplorer(result.transactionHash,"tx"));
+                }
+            }
+        }
+    );
+};
+
+const showResult = function(fee, hash){
+    console.log("Completion fee : " + fee +hash);
+    localStorage.setItem("completed", true);
+    localStorage.setItem("uuid",currentOrder.uuid);
+    localStorage.setItem("task_address", currentOrder.taskContractAddress);
+
+    console.log(localStorage.completed);
+    console.log(localStorage.uuid);
+    console.log(localStorage.task_address);
+    window.open("output.html"); //, "_self"
+};
+
+const waitingForTaskCompletion = function(){
+
+    //wait for completion
+    console.log("Waiting for task @ ", currentOrder.taskContractAddress, " to complete");
+    let completionEvent = nebulaAi.TaskCompleted();
+    completionEvent.watch(
+        function (error, result) {
+            if(error){
+                console.log(error);
+            }else{
+                if(result.args._task_owner.toLowerCase() === web3.eth.defaultAccount.toLowerCase()){
+                    completionEvent.stopWatching();
+                    let completion_fee = result.args._completion_fee;
+                    showResult(completion_fee,"--", result.transactionHash, "--",result);
+                    $('#taskCompleted').show();
+                    $("#complete_block").html(createLinkToExplorer(result.blockNumber, "block"));
+                    $('#complete_block_hash').html(createLinkToExplorer(result.blockHash, "block"));
+                    $('#task_compl_txhash').html(createLinkToExplorer(result.transactionHash, "tx"));
+                }
+
+            }
+        }
+    )
 };
 
 const payToken = function () {
-    let fee = parseFloat($("#amount").val());
+    let fee = parseFloat($("#tx_fee_value").val());
+
+    console.log(JSON.stringify(currentOrder.parameters));
+
     if (fee >= minimalFee) {
+
         currentOrder.fee = web3.toWei(fee, "ether");
+
         nebulaAi.submitTask(
             currentOrder.uuid,
+            currentOrder.name,
+            "",
+            currentOrder.scriptURI,
+            currentOrder.outputURI,
+            JSON.stringify(currentOrder.parameters),
             {
-                value : currentOrder.fee
+                value: currentOrder.fee
             },
-            function(error, result){
-                console.log("error: ",error);
-                console.log("result: ",result);
-                $("#payment").empty();
-
-                console.log("waiting for submit confirmation");
-
-                let uuidHex = web3.fromAscii(currentOrder.uuid);
-
-                let submitEvent = nebulaAi.taskSubmitted();
-                submitEvent.watch(function (error, result) {
-
-                    if(result.args.uuid === uuidHex){
-                        submitEvent.stopWatching();
-                        console.log("Current Task submitted" , result.args.uuid);
-                        console.log("result:" , result);
-
-                        attachLink("receipt", result.transactionHash);
-
-                        let dispatchEvent = nebulaAi.taskDispatched(function (error, result) {
-
-                            if(result.args.uuid === uuidHex){
-                                dispatchEvent.stopWatching();
-                                console.log("Current Task Dispatched : " , result.args.uuid);
-                                console.log("result:" , result);
-
-                                attachLink("dispatched" , result.transactionHash);
-
-                                let completeEvent = nebulaAi.taskCompleted(function (error, result) {
-
-                                    if(result.args.uuid === uuidHex){
-                                        completeEvent.stopWatching();
-                                        console.log("Current Task Completed : " , result.args.uuid);
-                                        console.log("result:" , result);
-
-                                        attachLink("completed" , result.transactionHash);
-
-                                        $("#payment").append("<div>We are going to transfer you to the output page<div>");
-                                        setTimeout(function () { window.open("history.html?uuid=" + currentOrder.uuid, "_self"); }, 500);
-                                    }else{
-                                        console.log("not me yet");
-                                    }
-                                });
-                            }else{
-                                console.log("not me yet");
-                            }
-                        });
-                    }else{
-                        console.log("not me yet");
-                    }
-                });
-            }
-        );
-    } else alert("Minimum fee is less than 2.06");
-
-    //window.open("output.html", "_self");
+            function (error, result) {
+                if(error){
+                    console.log(error);
+                }else{
+                    waitingForSubmitConfirmation(result);
+                    waitingForTaskDispatch();
+                    waitingForTaskStart();
+                    waitingForTaskCompletion();
+                }
+            });
+    }else alert("Minimum fee is less than 10 token, if you need more use the link below to get some token to try");
 };
+
+function isTaskStarted(){
+    taskContractInstance.started(function (error, result) {
+        if(error){
+            console.log(error);
+        }else{
+            blocks.task.started = result;
+        }
+    });
+}
+function isTaskOk(){
+    taskContractInstance.task_issue(function (error, result) {
+        if(error){
+            console.log(error);
+        }else{
+            blocks.task.has_issue = result;
+        }
+    });
+}
+function isTaskCompleted(){
+    taskContractInstance.completed(function (error, result) {
+        if(error){
+            console.log(error);
+        }else{
+            blocks.task.completed = result;
+        }
+    });
+}
+
+function myTaskWorker(){
+    taskContractInstance.worker(function (error, result) {
+        if(error){
+            console.log(error);
+        }else{
+            blocks.task.worker = result;
+            $("#task_worker_cell").empty().html(createLinkToExplorer(result,"address"));
+        }
+    });
+}
+
+function getTaskID() {
+    taskContractInstance.task_id(function (error, result) {
+        if(error){
+            console.log(error);
+        }else{
+            blocks.task.task_id = Number(result);
+            $("#task_id_cell").empty().html(blocks.task.task_id);
+        }
+    })
+}
+
+function getUuid(){
+    taskContractInstance.uuid(function (error, result) {
+        if(error){
+            console.log(error);
+        }else{
+            blocks.task.uuid = result;
+            $("#uuid_cell").empty().html(result);
+        }
+    })
+}
+
+
+
+function getTransaction(panel, block, hash) {
+    web3.eth.getTransaction(hash,function (error, result) {
+        if(error){
+            console.log(error);
+        }else{
+            block = result;
+            $(panel).show().empty().append(
+                "<h5>Transaction Details</h5>" +
+                "<table>" +
+                "<tr><td>Tx Hash : </td><td>"+createLinkToExplorer(result.hash,"tx")+"</td></tr>" +
+                "<tr><td>Block Number : </td><td>" + result.blockNumber+"</td></tr>" +
+                "<tr><td>Block Hash : </td><td>"+createLinkToExplorer(result.blockHash,"block")+"</td></tr>" +
+                "<tr><td>From Wallet : </td><td>"+createLinkToExplorer(result.from,"address")+"</td></tr>" +
+                "<tr><td>To Nebula Contract @: </td><td>"+createLinkToExplorer(result.to,"address")+"</td></tr>" +
+                "<tr><td>Gas Spent : </td><td>"+result.gas+"</td></tr>" +
+                "<tr><td>Gas Price : </td><td>"+ toEther(result.gasPrice) + "</td></tr>" +
+                "<tr><td>Fee : </td><td>"+ toEther(result.value)+"</td></tr>" +
+                "</table>"
+            );
+        }
+    });
+}
+function createLinkToExplorer(fill, type){
+    return "<a href='http://ec2-18-218-114-50.us-east-2.compute.amazonaws.com:8000/#/"+type+"/"+fill+"' target='_blank'>"+fill+"</a>"
+}
+
+function toEther(value){
+    return web3.fromWei(value, 'ether');
+}
+
